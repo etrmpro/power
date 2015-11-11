e84a828ad33c19c90b0f7a6a44004c7957c08b0e
Stephen Nimmo <stephennimmo@gmail.com>
2015-11-11 14:52:46 -0600

    init

---
+++ ercot/MIS/MISAppService.cs
@@ -0,0 +1,28 @@
+﻿using ERCOT.MIS.reports;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace ERCOT.MIS
+{
+    public class MISAppService
+    {
+        public List<ListReportTypeSchemaResReport> FetchReportTypes()
+        {
+            var url = "http://mis.ercot.com/misapp/servlets/IceMktRepListWS?";
+            using (var webClient = new WebClient())
+            {
+                var xml = webClient.DownloadString(url);
+                XmlSerializer serializer = new XmlSerializer(typeof(ListReportTypeSchemaRes));
+                ListReportTypeSchemaRes res = (ListReportTypeSchemaRes)serializer.Deserialize(new StringReader(xml));
+                return res.ReportTypeList.ToList();
+            }
+        }
+    }
+}
---
+++ ercot/MIS/reports/ListReportTypeSchemaRes.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERCOT.MIS.reports
+{
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.ercot.com/schema/2012-10/nodal/ListReportTypeSchemaRes")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.ercot.com/schema/2012-10/nodal/ListReportTypeSchemaRes", IsNullable = false)]
+    public partial class ListReportTypeSchemaRes
+    {
+
+        private ListReportTypeSchemaResReport[] reportTypeListField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("Report", IsNullable = false)]
+        public ListReportTypeSchemaResReport[] ReportTypeList
+        {
+            get
+            {
+                return this.reportTypeListField;
+            }
+            set
+            {
+                this.reportTypeListField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.ercot.com/schema/2012-10/nodal/ListReportTypeSchemaRes")]
+    public partial class ListReportTypeSchemaResReport
+    {
+
+        private ushort reportTypeIDField;
+
+        private string reportNameField;
+
+        private string securityClassificationField;
+
+        /// <remarks/>
+        public ushort ReportTypeID
+        {
+            get
+            {
+                return this.reportTypeIDField;
+            }
+            set
+            {
+                this.reportTypeIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string ReportName
+        {
+            get
+            {
+                return this.reportNameField;
+            }
+            set
+            {
+                this.reportNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string SecurityClassification
+        {
+            get
+            {
+                return this.securityClassificationField;
+            }
+            set
+            {
+                this.securityClassificationField = value;
+            }
+        }
+    }
+
+
+
+}
